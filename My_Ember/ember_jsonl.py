c5aa4d2008da8539b746ffc29bd81aec72e5ff78
cphuamao <cphuamao@ntu.edu.tw>
2019-08-14 09:36:25 +0000

    Generate EMBER data is finally done.

--- My_Ember/ember_jsonl.py
+++ My_Ember/ember_jsonl.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[19]:
+# In[18]:
 
 
 import pefile
@@ -18,11 +18,7 @@ import matplotlib.pyplot as plt
 import jsonlines
 import hashlib
 import time
-
-
-# In[65]:
-
-
+import lief
 from scipy import stats
 import pandas as pd
 
@@ -34,7 +30,7 @@ def Entropy(labels, base=2):
     return en
 
 
-# In[2]:
+# In[20]:
 
 
 def strings(filename, min=5):
@@ -52,255 +48,157 @@ def strings(filename, min=5):
             yield result
 
 
-# In[3]:
+# In[56]:
 
 
 def gen_general(mypath):   
-    gg = 0
-    arr = []
-    for qq in range(1):
-        dic = {}
-        gg += 1
-        if gg > 50:
-            break
-        try:
-            fullpath = mypath
-            pe = pefile.PE(fullpath)
-            dic = {}
-
-            # number of imported and exported function
-
-            try:
-                count = 0
-                for entry in pe.DIRECTORY_ENTRY_IMPORT:
-                    for imp in entry.imports:
-                        count += 1
-                dic['imports'] = count
-            except:
-                dic['imports'] = 0
-
-            try:
-                count = 0
-                for entry in pe.DIRECTORY_ENTRY_EXPORT:
-                    for imp in entry.imports:
-                        count += 1
-                dic['exports'] = count
-            except:
-                dic['exports'] = 0
-
-            # size and virtual size
-
-            vsize = 0
-            size = 0
-            for section in pe.sections:
-                vsize += section.Misc_VirtualSize
-                size += section.SizeOfRawData
-            dic['vsize'] = vsize
-            dic['size'] = size
-
-            # Debug sections or not
-            try:
-                tmp = pe.DIRECTORY_ENTRY_DEBUG
-                dic['has_debug'] = 1
-            except:
-                dic['has_debug'] = 0
-
-            # signature
-            try:
-                tmp = pe.VS_FIXEDFILEINFO[0].Signature
-                dic['has_signature'] = 1
-            except:
-                dic['has_signature'] = 0
-
-            # Resource
-
-            try:
-                tmp = pe.DIRECTORY_ENTRY_RESOURCE
-                dic['has_resources'] = 1
-            except:
-                dic['has_resources'] = 0
-
-            # TLS
-
-            try:
-                tmp = pe.DIRECTORY_ENTRY_TLS
-                dic['has_tls'] = 1
-            except:
-                dic['has_tls'] = 0
-
-            # relocations
-
-            try:
-                tmp = pe.DIRECTORY_ENTRY_BASERELOC
-                dic['has_relocations'] = 1
-            except:
-                dic['has_relocations'] = 0
-
-            # number of symbol QQ
-
-            dic['symbols'] = 0
-
-            arr.append(dic)
-
-        except:
-            continue
-    #general = arr[0]
-    return arr[0]
+    lief_binary = lief.parse(mypath)
+    bytez = bytearray(open(mypath, 'rb').read())
+            
+    if lief_binary is None:
+        return {
+            'size': len(bytez),
+            'vsize': 0,
+            'has_debug': 0,
+            'exports': 0,
+            'imports': 0,
+            'has_relocations': 0,
+            'has_resources': 0,
+            'has_signature': 0,
+            'has_tls': 0,
+            'symbols': 0
+        }
+    return{
+        'size': len(bytez),
+        'vsize': lief_binary.virtual_size,
+        'has_debug': int(lief_binary.has_debug),
+        'exports': len(lief_binary.exported_functions),
+        'imports': len(lief_binary.imported_functions),
+        'has_relocations': int(lief_binary.has_relocations),
+        'has_resources': int(lief_binary.has_resources),
+        'has_signature': int(lief_binary.has_signature),
+        'has_tls': int(lief_binary.has_tls),
+        'symbols': len(lief_binary.symbols),
+    }
 
 
-# # Strings
 
-# In[81]:
-
-
-def gen_strings(mypath):    
+def gen_strings(mypath):
     npsl = []
     for qq in range(1):
         fullpath = mypath
         sl = list(strings(fullpath))
         npsl.append(sl)
-    # MZ
+    test = len(npsl[0])
+    allstrings = []
+    for i in range(test):
+        allstrings.append(str.encode(npsl[0][i]))
+    if allstrings:
+        # statistics about strings:
+        string_lengths = [len(s) for s in allstrings]
+        avlength = sum(string_lengths) / len(string_lengths)
+        # map printable characters 0x20 - 0x7f to an int array consisting of 0-95, inclusive
+        as_shifted_string = [np.abs(b - ord(b'\x20')) for b in b''.join(allstrings)]
+        c = np.bincount(as_shifted_string, minlength=96)  # histogram count
+        # distribution of characters in printable strings
+        csum = c.sum()
+        p = c.astype(np.float32) / csum
+        wh = np.where(c)[0]
+        H = np.sum(-p[wh] * np.log2(p[wh]))  # entropy
+    else:
+        avlength = 0
+        c = np.zeros((96,), dtype=np.float32)
+        H = 0
+        csum = 0
+    
     num = 0
     MZ = 0
     for obj in npsl[num]:
         if 'MZ' in obj:
             MZ += 1
-            #print(obj)
-
-    # avglength
-
-    tl = 0
-    for obj in npsl[num]:
-        tl += len(obj)
-    avlength = tl/len(npsl[num])
-
-    #numstrings
-
-    numstrings = len(npsl[num])
-
-    # urls
-
     urls = 0
     for obj in npsl[num]:
         if 'https://' in obj or 'http://' in obj:
             urls += 1
-
-    # paths
-
-    paths = 0
-    for obj in npsl[num]:
-        if 'C:\\' == obj[0:3] or 'D:\\' == obj[0:3]:
-            paths += 1
-
-    # printables
-
-    printables = 0
-    for obj in npsl[num]:
-        n = len(obj)
-        for i in range(n-5):
-            if obj[i:(i+5)].isprintable():
-                printables += 1
-                break
-
-    # entropy
-
-    a = ''.join(npsl[num])
-
-
-    def entropys(string):
-            "Calculates the Shannon entropy of a string"
-
-            # get probability of chars in string
-            prob = [ float(string.count(c)) / len(string) for c in dict.fromkeys(list(string)) ]
-
-            # calculate the entropy
-            entropy = - sum([ p * math.log(p) / math.log(2.0) for p in prob ])
-
-            return entropy
-    entropy = entropys(npsl[num])
-
-    # printabledist
-
-    printablehist = []
-    for obj in npsl[num]:
-        n = len(obj)
-        for i in range(n-5):
-            if obj[i:(i+5)].isprintable():
-                printablehist.append((obj))
-                break
-    printabledist = hist(printablehist,96)[0]
-
-    # registry
-
     registry = 0
     for obj in npsl[num]:
         if 'HKEY_ ' in obj:
             registry += 1
             print(obj)
-
-    # make dictionary
-    strs = {}
-
-    title = ['MZ',
-    'avlength',
-    'entropy',
-    'numstrings',
-    'paths',
-    'printabledist',
-    'printables',
-    'registry',
-    'urls']
-
-    strs['MZ'] = MZ
-    strs['avlength'] = avlength
-    strs['entropy'] = entropy
-    strs['numstrings'] = numstrings
-    strs['paths'] = paths
-    strs['printabledist'] = list(printabledist)
-    strs['printables'] = printables
-    strs['registry'] = registry
-    strs['urls'] = urls
-    return strs
+    paths = 0
+    for obj in npsl[num]:
+        if 'C:\\' == obj[0:3] or 'D:\\' == obj[0:3]:
+            paths += 1
+            
+    
+    return {
+        'numstrings': len(allstrings),
+        'avlength': avlength,
+        'printabledist': c.tolist(),  # store non-normalized histogram
+        'printables': int(csum),
+        'entropy': float(H),
+        'paths': paths,
+        'urls': urls,
+        'registry': registry,
+        'MZ': MZ
+    }
+    return allstrings
 
 
 # # Histogram
 
-# In[5]:
+# In[54]:
 
 
 def gen_hist(mypath):    
-    content = bytearray(open(mypath, 'rb').read())
-    freq, bins, patches = plt.hist(content, 256)
-    #histogram = freq
-    return list(freq)
+    bytez = bytearray(open(mypath, 'rb').read())
+    counts = np.bincount(np.frombuffer(bytez, dtype=np.uint8), minlength=256)
+    return counts.tolist()
 
 
-# # Combine
+# In[64]:
 
-# output = []
-# keys_strings = ['avlength', 'numstrings', 'registry', 'urls', 'MZ', 'printables', 'entropy', 'paths', 'printabledist']
-# keys_general = ['exports', 'has_resources', 'imports', 'symbols', 'has_signature', 'has_relocations', 'has_debug', 'vsize', 'size', 'has_tls']
-# for obj in range(1):
-#     feature_num = []
-#     for i in range(3):
-#         feature_num.append([])
-#     for j in keys_strings:
-#         if j != 'printabledist':
-#             feature_num[0].append(strs[j])
-#         else:
-#             for k in range(96):
-#                 feature_num[0].append(strs[j][k])
-#     for i in range(256):
-#         feature_num[1].append(histogram[i])
-#     for i in keys_general:
-#         feature_num[2].append(general[i])
-#     for i in range(256 - 104):
-#         feature_num[0].append(0)
-#     for i in range(256 - 10):
-#         feature_num[2].append(0)
-#     output.append(feature_num)
 
-# In[6]:
+def gen_byteentropy(mypath):
+    lief_binary = lief.parse(mypath)    
+    bytez = bytearray(open(mypath, 'rb').read())
+    name = 'byteentropy'
+    dim = 256
+    window = 2048
+    step = 1024
+    def _entropy_bin_counts(block):
+        # coarse histogram, 16 bytes per bin
+        c = np.bincount(block >> 4, minlength=16)  # 16-bin histogram
+        p = c.astype(np.float32) / window
+        wh = np.where(c)[0]
+        H = np.sum(-p[wh] * np.log2(
+            p[wh])) * 2  # * x2 b.c. we reduced information by half: 256 bins (8 bits) to 16 bins (4 bits)
+
+        Hbin = int(H * 2)  # up to 16 bins (max entropy is 8 bits)
+        if Hbin == 16:  # handle entropy = 8.0 bits
+            Hbin = 15
+
+        return Hbin, c
+
+    output = np.zeros((16, 16), dtype=np.int)
+    a = np.frombuffer(bytez, dtype=np.uint8)
+    if a.shape[0] < window:
+        Hbin, c = _entropy_bin_counts(a)
+        output[Hbin, :] += c
+    else:
+        # strided trick from here: http://www.rigtorp.se/2011/01/01/rolling-statistics-numpy.html
+        shape = a.shape[:-1] + (a.shape[-1] - window + 1, window)
+        strides = a.strides + (a.strides[-1],)
+        blocks = np.lib.stride_tricks.as_strided(a, shape=shape, strides=strides)[::step, :]
+
+        # from the blocks, compute histogram
+        for block in blocks:
+            Hbin, c = _entropy_bin_counts(block)
+            output[Hbin, :] += c
+
+    return output.flatten().tolist()
+
+# In[24]:
 
 
 def gen_ie(mypath):    
@@ -326,7 +224,7 @@ def gen_ie(mypath):
     return imports, exports
 
 
-# In[12]:
+# In[25]:
 
 
 def gen_sha256(mypath):
@@ -342,7 +240,7 @@ def gen_sha256(mypath):
     return sha
 
 
-# In[31]:
+# In[26]:
 
 
 def gen_appeared(mypath):
@@ -377,59 +275,96 @@ def gen_appeared(mypath):
     return year+"-"+month
 
 
-# In[83]:
+# In[78]:
 
 
 def gen_section(mypath):
-    pe = pefile.PE(mypath)
-   # print(pe.sections[0])
-    en = False
-    eop = pe.OPTIONAL_HEADER.AddressOfEntryPoint
-    sec = {}
-    sec['sections'] = []
-    '''
-    for section in pe.sections:
-        print(section.Name, section.Misc, section.Misc_PhysicalAddress,
-              section.Misc_VirtualSize, section.VirtualAddress, section.SizeOfRawData,
-              section.PointerToRawData, section.PointerToRelocations, section.PointerToLinenumbers,
-              section.NumberOfRelocations, section.NumberOfLinenumbers, section.Characteristics)
-    '''
-    for section in pe.sections:
-        arr = [section.Misc, section.Misc_PhysicalAddress,
-              section.Misc_VirtualSize, section.VirtualAddress, section.SizeOfRawData,
-              section.PointerToRawData, section.PointerToRelocations, section.PointerToLinenumbers,
-              section.NumberOfRelocations, section.NumberOfLinenumbers, section.Characteristics]
-        #print(arr)
-        tmp = {}
-        tmp['name'] = (section.Name).decode('utf-8')[:5]
-        tmp['vsize'] = section.Misc_VirtualSize
-        tmp['size'] = section.SizeOfRawData
-        tmp['entropy'] = Entropy(arr)
-        sec['sections'].append(tmp)
-        
-    
-    for section in pe.sections:
-        if section.contains_rva(eop):
-            sec['entry'] = section
-            en = True
-    if not en:
-        sec['entry'] = None
-    return sec
+    lief_binary = lief.parse(mypath)
+    if lief_binary is None:
+        return {"entry": "", "sections": []}
+    def properties(s):
+        return [str(c).split('.')[-1] for c in s.characteristics_lists]
+    # properties of entry point, or if invalid, the first executable section
+    try:
+        entry_section = lief_binary.section_from_offset(lief_binary.entrypoint).name
+    except lief.not_found:
+        # bad entry point, let's find the first executable section
+        entry_section = ""
+        for s in lief_binary.sections:
+            if lief.PE.SECTION_CHARACTERISTICS.MEM_EXECUTE in s.characteristics_lists:
+                entry_section = s.name
+                break
+
+    raw_obj = {"entry": entry_section}
+    raw_obj["sections"] = [{
+        'name': s.name,
+        'size': s.size,
+        'entropy': s.entropy,
+        'vsize': s.virtual_size,
+        'props': properties(s)
+    } for s in lief_binary.sections]
+    return raw_obj
 
 
-# In[ ]:
+# In[28]:
 
 
-["Names","Misc","Misc_PhysicalAddress","Misc_VirtualSize",VirtualAddress,SizeOfRawData,PointerToRawData,
- PointerToRelocations,PointerToLinenumbers,NumberOfRelocations,NumberOfLinenumbers,Characteristics]
+def gen_header(mypath):
+    lief_binary = lief.parse(mypath)
+    raw_obj = {}
+    raw_obj['coff'] = {'timestamp': 0, 'machine': "", 'characteristics': []}
+    raw_obj['optional'] = {
+        'subsystem': "",
+        'dll_characteristics': [],
+        'magic': "",
+        'major_image_version': 0,
+        'minor_image_version': 0,
+        'major_linker_version': 0,
+        'minor_linker_version': 0,
+        'major_operating_system_version': 0,
+        'minor_operating_system_version': 0,
+        'major_subsystem_version': 0,
+        'minor_subsystem_version': 0,
+        'sizeof_code': 0,
+        'sizeof_headers': 0,
+        'sizeof_heap_commit': 0
+    }
+    if lief_binary is None:
+        return raw_obj
+    raw_obj['coff']['timestamp'] = lief_binary.header.time_date_stamps
+    raw_obj['coff']['machine'] = str(lief_binary.header.machine).split('.')[-1]
+    raw_obj['coff']['characteristics'] = [str(c).split('.')[-1] for c in lief_binary.header.characteristics_list]
+    raw_obj['optional']['subsystem'] = str(lief_binary.optional_header.subsystem).split('.')[-1]
+    raw_obj['optional']['dll_characteristics'] = [str(c).split('.')[-1] for c in lief_binary.optional_header.dll_characteristics_lists]
+    raw_obj['optional']['magic'] = str(lief_binary.optional_header.magic).split('.')[-1]
+    raw_obj['optional']['major_image_version'] = lief_binary.optional_header.major_image_version
+    raw_obj['optional']['minor_image_version'] = lief_binary.optional_header.minor_image_version
+    raw_obj['optional']['major_linker_version'] = lief_binary.optional_header.major_linker_version
+    raw_obj['optional']['minor_linker_version'] = lief_binary.optional_header.minor_linker_version
+    raw_obj['optional']['major_operating_system_version'] = lief_binary.optional_header.major_operating_system_version
+    raw_obj['optional']['minor_operating_system_version'] = lief_binary.optional_header.minor_operating_system_version
+    raw_obj['optional']['major_subsystem_version'] = lief_binary.optional_header.major_subsystem_version
+    raw_obj['optional']['minor_subsystem_version'] = lief_binary.optional_header.minor_subsystem_version
+    raw_obj['optional']['sizeof_code'] = lief_binary.optional_header.sizeof_code
+    raw_obj['optional']['sizeof_headers'] = lief_binary.optional_header.sizeof_headers
+    raw_obj['optional']['sizeof_heap_commit'] = lief_binary.optional_header.sizeof_heap_commit
+    return raw_obj
 
 
-# In[76]:
+# In[75]:
 
 
-def gen_total(mypath):    
+def gen_total(mypath,label):    
     total = {}
-
+    if label == 'B' or label == 'b':
+        total['label'] = 0
+    elif label == 'M' or label == 'm':
+        total['label'] = 1
+    elif label == 'N' or label == 'n':
+        total['label'] = -1
+    else:
+        print('Please enter correct label !!!')
+    
     total['general'] = gen_general(mypath)
     total['strings'] = gen_strings(mypath)
     total['histogram'] = gen_hist(mypath)
@@ -437,6 +372,9 @@ def gen_total(mypath):
     total['sha256'] = gen_sha256(mypath)
     total['appeared'] = gen_appeared(mypath)
     total['section'] = gen_section(mypath)
+    total['header'] = gen_header(mypath)
+    total['byteentropy'] = gen_byteentropy(mypath)
+    
     return total
 
 
@@ -444,30 +382,29 @@ def gen_total(mypath):
 
 
 if __name__ == '__main__':
+    label = sys.argv[1]
     json_save = []
     argc = len(sys.argv)
     df = True
-    for i in range(1, argc):
+    for i in range(2, argc):
         if isdir(sys.argv[i]):
             df = False
         
     if df:
-        for i in range(1,argc):
-            mypath = sys.argv[1]
-            tmp = gen_total(mypath)
+        for i in range(2,argc):
+            mypath = sys.argv[i]
+            tmp = gen_total(mypath,label)
             json_save.append(tmp)
             with jsonlines.open('output.jsonl', mode='w') as writer:
                 writer.write(json_save)
-    elif (not df):
+    elif (not df) and argc == 3:
         files = listdir(fpath)
         for f in files:
             mypath = fpath + '/' + f
-            tmp = gen_total(mypath)
+            tmp = gen_total(mypath,label)
             json_save.append(tmp)
         with jsonlines.open('output.jsonl', mode='w') as writer:
             writer.write(json_save)
     else:
-        print("\nPlease enter files' name seperated by space or a direction name ! \n")
-
-
+        print("\nPlease first enter B/M/N(benignware or malware or No label) and second enter files' name seperated by space or a direction name ! \n")
 
